08f662af18b3cf4907a0f5c583c27f94976192d1
tplovejava <tplovejava@sina.cn>
2019-03-10 14:05:43 +0800

    分布式事务

--- tpjob-bussiness/tpjob-security/src/main/java/com/tp/soft/SecurityRunMain.java
+++ tpjob-bussiness/tpjob-security/src/main/java/com/tp/soft/SecurityRunMain.java
@@ -1,13 +1,18 @@
 package com.tp.soft;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringCloudApplication
 @EnableEurekaClient
 @MapperScan("com.tp.soft.*.dao")
+@EnableDistributedTransaction
+@EnableAutoConfiguration(exclude = {DruidDataSourceAutoConfigure.class})
 public class SecurityRunMain {
 
     public static void main(String[] args) {
